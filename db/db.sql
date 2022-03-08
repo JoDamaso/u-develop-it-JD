8c2f1041d0586b7972d1551d1f1a34f39d658957
JoDamaso <darryldamaso@gmail.com>
2022-03-08 15:56:24 -0600

    dependencies, express, mysql2 added

--- db/db.sql
+++ db/db.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS election;
 CREATE DATABASE election;
 USE election;
 
+-- first source db/db.sql
+-- second source db/schema.sql
+-- third source db/seeds.sql 
+-- this creates your database, then table, then inserts input via the seeds.sql 
+-- having these files means you only have to run it. 
