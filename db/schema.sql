c37b225404432cd0f3497191f35e630c1ccd01a9
JoDamaso <darryldamaso@gmail.com>
2022-03-09 14:23:38 -0600

    table for parties added

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,16 @@
+-- Creating candidates table
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL
+);
+
+-- Creating parties table
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+    -- TEXT can store much longer strings rather than VARCHAR
+    -- TEXT uses max space when used, so becareful always using it
 );
