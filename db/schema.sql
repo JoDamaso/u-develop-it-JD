d0524daf77bc6544a75f7213464825573c6e9e0f
JoDamaso <darryldamaso@gmail.com>
2022-03-09 18:23:30 -0600

    voters table added with timestamps

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 -- This will drop/delete the tables every time you run the schema.sql file, ensuring that you start with a clean slate.
 
 -- Creating parties table
@@ -21,4 +22,12 @@ CREATE TABLE candidates (
     industry_connected BOOLEAN NOT NULL,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
     -- party_id is a foreign key that refers to parties table 'id', if a party is deleted, it is set to NULL
+);
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
